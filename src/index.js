961f15070636091c03cfc61a384cf99a47487b3b
Zmiter <ozmiter@gmail.com>
2021-03-15 13:07:07 +0300

    feature: add temp solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  //let brackets = 	bracketsConfig.flat().join('')
+  let brackets = "[]()"
+  let stackArr = []
+
+  for(let bracket of str) {
+    let bracketsIndex = brackets.indexOf(bracket)
+
+    if (bracketsIndex === -1){
+      continue
+    }
+
+    if(bracketsIndex % 2 === 0) {
+      stackArr.push(bracketsIndex + 1)
+    } else {
+      if(stackArr.pop() !== bracketsIndex) {
+        return false;
+      }
+    }
+  }
+  return stackArr.length === 0
 }
